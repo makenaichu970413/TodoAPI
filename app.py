cd8fbccfd917a23bc70418ad81fe804a5e0ec7d8
makenaichuweb <makenaichuweb@gmail.com>
2022-07-12 17:22:37 +0800

    6.1th commit

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config["MAIL_PORT"] = int(os.environ.get("MAIL_PORT")) # 405
 app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
 app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+# app.config["MAIL_USE_SSL"] = False
 mail = Mail(app)
 
 # Token Secret
